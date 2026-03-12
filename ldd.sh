ac61f3f842fdce5153f60c618e1e00eefda7e936
qin <3435312235@qq.com>
2026-03-12 11:41:38 +0800

    Update ldd.sh

--- ldd.sh
+++ ldd.sh
@@ -5,7 +5,7 @@ executable="$1"
 lib_dir="$2"
 
 # 下载依赖库
-sudo apt-get install libqt5serialport5-dev libqt5multimedia5-dev
+sudo apt-get install libqt5serialport5-dev qtmultimedia5-dev
 
 # 检查可执行文件是否存在
 if [[ ! -f "$executable" ]]; then
